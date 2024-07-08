643498d2ea5266ad521288430c1238e55ffc9426
Akshat2512 <akshat.2016603@gmail.com>
2024-07-08 22:13:26 +0530

    commit

--- HTML/myscript.js
+++ HTML/myscript.js
@@ -1,8 +1,7 @@
 
 const owner = 'Akshat2512';
 const repo = 'Crime-Prediction';
-
- // 'Authorization' : 'Bearer ' + token,
+const token = '';  // 'Authorization' : 'Bearer ' + token,
 const apiUrl = `https://api.github.com/repos/${owner}/${repo}/actions/workflows`;
 var header = { 'Accept' : 'application/vnd.github.v3+json','Authorization' : 'Bearer ' + token,'X-GitHub-Api-Version' : '2022-11-28'};
 document.querySelector('#btn1').onclick = async ()=>{
