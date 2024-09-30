d84b9b91a83be86d47ed975a89e3e9321dc861de
Akshat2512 <akshat.2016603@gmail.com>
2024-09-30 05:50:42 +0530

    deploy

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ db_cred = sys.argv
 app = Flask(__name__)
 
 def connect_database():
-    connection = MySQLdb.connect(host=db_cred[1],
-                             port=int(db_cred[2]),
-                             user=db_cred[3],
-                             password=db_cred[4],
-                             db=db_cred[5],
-                             cursorclass=MySQLdb.cursors.DictCursor)
+    # connection = MySQLdb.connect(host=db_cred[1],   // ssh tunnel connection
+    #                          port=int(db_cred[2]),
+    #                          user=db_cred[3],
+    #                          password=db_cred[4],
+    #                          db=db_cred[5],
+    #                          cursorclass=MySQLdb.cursors.DictCursor)
 
     # connection = MySQLdb.connect(host='localhost',
     #                              port=3306,
