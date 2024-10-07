0a7ece4dca083bcaa1ac45b0b2405dcd38a018ed
Akshat2512 <akshat.2016603@gmail.com>
2024-10-08 00:02:22 +0530

    deploy

--- api/app.py
+++ api/app.py
@@ -234,6 +234,10 @@ app = Flask(__name__)
 #      return "exist"
 #     return "not exist"
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+    # return render_template('login.html')
 
 @app.route('/run_1', methods=['POST'])
 def run_1():
