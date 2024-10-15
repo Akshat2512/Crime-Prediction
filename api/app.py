e8aeb0d4bcea258daec3e13ad9cec9b13351b2d4
Akshat2512 <akshat.2016603@gmail.com>
2024-10-15 18:04:41 +0530

    deploy

--- api/app.py
+++ api/app.py
@@ -1,14 +1,24 @@
-import subprocess,json
+import subprocess,json, os
 from flask import Flask, render_template, request, redirect
 from Module.CrimePrediction import prediction
 from Module.currentcrime import live_crime_pred
 from Module.filterdata import filter_data_for_Analysis
+
 # # from flask_mysqldb import MySQL
 # import MySQLdb.cursors
 
 
 app = Flask(__name__)
 
+
+@app.route('/env', methods=['POST'])
+def read_env():
+    postgres_url = os.getenv('POSTGRES_URL')
+    postgres_user = os.getenv('POSTGRES_USER')
+    postgres_host = os.getenv('POSTGRES_HOST')
+    postgres_password = os.getenv('POSTGRES_PASSWORD')
+    postgres_database = os.getenv('POSTGRES_DATABASE')
+    return render_template(f'{postgres_url}, {postgres_user}')
 
 @app.route('/run_1', methods=['POST'])
 def run_1():
