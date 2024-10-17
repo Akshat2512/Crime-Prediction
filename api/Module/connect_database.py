88efd326d86d0f734ab8434bae4524c99518bde4
Akshat2512 <akshat.2016603@gmail.com>
2024-10-17 22:40:09 +0530

    commit

---
+++ api/Module/connect_database.py
@@ -0,0 +1,40 @@
+import psycopg2, os
+def retrieve_database(query):
+    connection = psycopg2.connect("postgres://postgres:password@localhost:5432/verceldb")
+    # connection = psycopg2.connect(os.getenv('POSTGRES_URL'))
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+    output = cursor.fetchall()
+   
+    cursor.close()
+    connection.close()
+    return output
+
+
+
+def update_database(query):
+
+    connection = psycopg2.connect("postgres://postgres:password@localhost:5432/verceldb")
+    # connection = psycopg2.connect(os.getenv('POSTGRES_URL'))
+    connection.autocommit = True
+    cursor = connection.cursor()
+    cursor.execute(query)
+   
+    cursor.close()
+    connection.close()
+
+# CREATE TABLE users (  
+#               id SERIAL PRIMARY KEY ,
+#               first_name VARCHAR(30), 
+#               middle_name VARCHAR(30), 
+#               last_name VARCHAR(30), 
+#               age INT, 
+#               phone BIGINT, 
+#               email VARCHAR(30), 
+#               country VARCHAR(5), 
+#               username VARCHAR(30), 
+#               passhash VARCHAR, 
+#               salt VARCHAR,
+#               sess_id VARCHAR,
+#               sess_start TIMESTAMP);
