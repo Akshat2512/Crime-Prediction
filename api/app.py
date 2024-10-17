102ae12377ef684ac4abc2216a9ae4b7e40ff26c
Akshat2512 <akshat.2016603@gmail.com>
2024-10-17 20:07:50 +0530

    deploy

--- api/app.py
+++ api/app.py
@@ -250,5 +250,5 @@ def get_current_crime():
 
     return output
 
-if __name__ == '__main__':
-        app.run(host='localhost', port=5000, debug=True)
+# if __name__ == '__main__':
+#         app.run(host='localhost', port=5000, debug=True)
